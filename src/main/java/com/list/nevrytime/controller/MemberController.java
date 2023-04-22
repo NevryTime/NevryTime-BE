1ac51c55571399138ce9e190c63fe13bf80d4228
List <namest504@gmail.com>
2023-04-22 17:42:10 +0900

    feat : 로그인 컨트롤러 추가

---
+++ src/main/java/com/list/nevrytime/controller/MemberController.java
@@ -0,0 +1,28 @@
+package com.list.nevrytime.controller;
+
+import com.list.nevrytime.controller.dto.MemberResponseDto;
+import com.list.nevrytime.service.MemberService;
+import com.list.nevrytime.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/me")
+    public ResponseEntity<MemberResponseDto> findMemberInfoById() {
+        return ResponseEntity.ok(memberService.findMemberInfoById(SecurityUtil.getCurrentMemberId()));
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<MemberResponseDto> findMemberInfoByEmail(@PathVariable String email) {
+        return ResponseEntity.ok(memberService.findMemberInfoByEmail(email));
+    }
+}
