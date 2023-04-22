6fefc4fe08c2243a4772bdedeb2e59652cf5dac8
List <namest504@gmail.com>
2023-04-22 21:13:02 +0900

    refactor : DTO 패키지 경로 수정 및 게시판 종류 DTO 추가

--- src/main/java/com/list/nevrytime/jwt/TokenProvider.java
+++ src/main/java/com/list/nevrytime/jwt/TokenProvider.java
@@ -1,6 +1,6 @@
 package com.list.nevrytime.jwt;
 
-import com.list.nevrytime.controller.dto.TokenDto;
+import com.list.nevrytime.dto.TokenDto;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
