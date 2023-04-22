c4414df880d5f545797b7f7144a94b4474f60818
List <namest504@gmail.com>
2023-04-22 17:21:22 +0900

    chore: 프로젝트 초기 구조 설정

---
+++ src/main/java/com/list/nevrytime/NevrytimeApplication.java
@@ -0,0 +1,13 @@
+package com.list.nevrytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NevrytimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NevrytimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/list/nevrytime/NevrytimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.list.nevrytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NevrytimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
