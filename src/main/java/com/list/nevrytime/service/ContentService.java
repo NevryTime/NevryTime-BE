d5840276d8ff0b34855e634e344460334c774a97
List <namest504@gmail.com>
2023-04-23 17:30:30 +0900

    feat : 게시글 컨트롤러, 서비스 패키지 생성

---
+++ src/main/java/com/list/nevrytime/service/ContentService.java
@@ -0,0 +1,16 @@
+package com.list.nevrytime.service;
+
+import com.list.nevrytime.repository.ContentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ContentService {
+
+    private final ContentRepository contentRepository;
+
+
+}
