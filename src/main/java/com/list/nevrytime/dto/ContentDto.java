38bbd389a382eeab8e5f97d1c95a94787c999beb
List <namest504@gmail.com>
2023-04-24 19:39:26 +0900

    refactor : ContentDTO 수정

--- src/main/java/com/list/nevrytime/dto/ContentDto.java
+++ src/main/java/com/list/nevrytime/dto/ContentDto.java
@@ -5,6 +5,7 @@ import com.list.nevrytime.entity.Content;
 import com.list.nevrytime.entity.Member;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -18,18 +19,18 @@ public class ContentDto {
     public static class ContentResponseDto {
 
         private Long id;
-        private Board board;
-        private Member member;
+        private String boardName;
+        private String userName;
         private String title;
         private String content;
         private int likes;
         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
         private LocalDateTime createAt;
         private boolean isImage;
-        private boolean Show;
+        private boolean isShow;
 
         public static ContentResponseDto of(Content content) {
-            return new ContentResponseDto(content.getId(), content.getBoard(),content.getMember(),content.getTitle(),content.getContent(), content.getLikes(), content.getCreateAt(), content.isImage(), content.isShow());
+            return new ContentResponseDto(content.getId(), content.getBoard().getName(),content.getMember().getName(),content.getTitle(),content.getContent(), content.getLikes(), content.getCreateAt(), content.isImage(), content.isShow());
         }
     }
 
@@ -37,10 +38,16 @@ public class ContentDto {
     @Setter
     @AllArgsConstructor
     @NoArgsConstructor
-    public class ContentRequestDto {
+    public static class ContentCreateRequestDto {
 
-        private Long id;
         private String title;
         private String content;
+        private Long boardId;
+        private Long memberId;
+        @Nullable
+        private boolean isImage;
+        @Nullable
+        private boolean isShow;
     }
+
 }
