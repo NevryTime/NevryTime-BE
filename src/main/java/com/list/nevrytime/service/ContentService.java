1d6ac13649b7c1f20a541fedef5974fe529ac821
List <namest504@gmail.com>
2023-04-25 00:09:10 +0900

    feat : 게시글 전체 조회 및 삭제 로직 구현

--- src/main/java/com/list/nevrytime/service/ContentService.java
+++ src/main/java/com/list/nevrytime/service/ContentService.java
@@ -1,5 +1,6 @@
 package com.list.nevrytime.service;
 
+import com.list.nevrytime.dto.BoardDto;
 import com.list.nevrytime.dto.ContentDto;
 import com.list.nevrytime.dto.ContentDto.ContentCreateRequestDto;
 import com.list.nevrytime.entity.Board;
@@ -9,10 +10,13 @@ import com.list.nevrytime.repository.BoardRepository;
 import com.list.nevrytime.repository.ContentRepository;
 import com.list.nevrytime.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.list.nevrytime.dto.ContentDto.*;
 
@@ -21,6 +25,7 @@ import static com.list.nevrytime.dto.ContentDto.*;
 @Transactional(readOnly = true)
 public class ContentService {
 
+    private final ModelMapper modelMapper;
     private final BoardRepository boardRepository;
     private final MemberRepository memberRepository;
     private final ContentRepository contentRepository;
@@ -44,5 +49,23 @@ public class ContentService {
                 .build();
 
         return ContentResponseDto.of(contentRepository.save(content));
+    }
+
+    @Transactional
+    public List<ContentResponseDto> findAllContent() {
+        List<Content> contents = contentRepository.findAll();
+        List<ContentResponseDto> resultList = contents
+                .stream()
+                .map(list -> modelMapper.map(list, ContentResponseDto.class))
+                .collect(Collectors.toList());
+        return resultList;
+    }
+
+    @Transactional
+    public ContentDeleteResponseDto deleteContentByName(Long contentId) {
+        contentRepository.deleteById(contentId);
+        ContentDeleteResponseDto contentDeleteResponseDto = new ContentDeleteResponseDto();
+        contentDeleteResponseDto.setSuccess(true);
+        return contentDeleteResponseDto;
     }
 }
