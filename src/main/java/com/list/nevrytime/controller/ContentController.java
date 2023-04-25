a42e819fda26adf016945d1cd7c6429606df25b3
List <namest504@gmail.com>
2023-04-25 14:44:22 +0900

    refactor : 코드 리팩토링

--- src/main/java/com/list/nevrytime/controller/ContentController.java
+++ src/main/java/com/list/nevrytime/controller/ContentController.java
@@ -1,6 +1,5 @@
 package com.list.nevrytime.controller;
 
-import com.list.nevrytime.dto.BoardDto;
 import com.list.nevrytime.dto.ContentDto.ContentCreateRequestDto;
 import com.list.nevrytime.service.ContentService;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +24,7 @@ public class ContentController {
     public ResponseEntity<ContentDeleteResponseDto> deleteContentByName(@PathVariable Long contentId ) {
         return ResponseEntity.ok(contentService.deleteContentByName(contentId));
     }
+
     @PostMapping("/create")
     public ResponseEntity<ContentResponseDto> createContent(@RequestBody ContentCreateRequestDto contentCreateRequestDto) {
         return ResponseEntity.ok(contentService.createContent(contentCreateRequestDto));
