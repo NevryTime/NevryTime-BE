f374a6014ca9529a736f3446c7dfb38b799a63c5
List <namest504@gmail.com>
2023-04-27 21:56:53 +0900

    feat : 댓글 Entity 추가

--- src/main/java/com/list/nevrytime/entity/Comment.java
+++ src/main/java/com/list/nevrytime/entity/Comment.java
@@ -1,23 +1,44 @@
 package com.list.nevrytime.entity;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "comment")
 @Getter
 @Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Comment {
 
     @Id
     @Column(name = "comment_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "content_id")
+    private Content content;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String commentContent;
+
+    @ColumnDefault("0")
+    private Long parentId;
+    @ColumnDefault("0")
+    private int depth;
+
+
+    private LocalDateTime createAt;
 
 
 }
