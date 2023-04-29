44f490895685f16a8b238d4d7967cb80b9472404
List <namest504@gmail.com>
2023-04-30 01:47:57 +0900

    feat : 패스워드 변경 API 구현 및 로그인, 회원가입 로직 변경

--- src/main/java/com/list/nevrytime/controller/AuthController.java
+++ src/main/java/com/list/nevrytime/controller/AuthController.java
@@ -1,13 +1,19 @@
 package com.list.nevrytime.controller;
 
-
-import com.list.nevrytime.dto.MemberDto;
-import com.list.nevrytime.dto.TokenDto;
 import com.list.nevrytime.dto.TokenDto.TokenRequestDto;
+import com.list.nevrytime.entity.Member;
+import com.list.nevrytime.exception.CustomException;
+import com.list.nevrytime.security.jwt.MemberPrincipal;
 import com.list.nevrytime.service.AuthService;
+import com.list.nevrytime.service.JwtService;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 import static com.list.nevrytime.dto.MemberDto.*;
 import static com.list.nevrytime.dto.TokenDto.*;
@@ -16,26 +22,52 @@ import static com.list.nevrytime.dto.TokenDto.*;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
     private final AuthService authService;
+    private final JwtService jwtService;
 
     @PostMapping("/signup")
-    public ResponseEntity<MemberResponseDto> signup(@RequestBody MemberRequestDto memberRequestDto) {
-        return ResponseEntity.ok(authService.signup(memberRequestDto));
+    public RegisterResponseDto register(@RequestBody @Valid RegisterRequestDto registerRequestDto) {
+        Member member = authService.register(registerRequestDto);
+        return new RegisterResponseDto(true, member.getName());
     }
 
+
     @PostMapping("/login")
-    public ResponseEntity<TokenResponseDto> login(@RequestBody MemberRequestDto memberRequestDto) {
-        return ResponseEntity.ok(authService.login(memberRequestDto));
+    public LoginResponseDto login(@RequestBody @Valid LoginDto loginDto) {
+        Long loginId = authService.login(loginDto);
+        String accessToken = jwtService.createAccessToken(loginId);
+        String refreshToken = jwtService.createRefreshToken(loginId);
+        authService.saveRefreshToken(loginId, refreshToken);
+        return new LoginResponseDto(true, loginId,accessToken, refreshToken);
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<TokenResponseDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
-        return ResponseEntity.ok(authService.reissue(tokenRequestDto));
+    public UseRefreshTokenResponseDto useRefreshToken(@RequestBody @Valid UseRefreshTokenRequestDto useRefreshTokenRequestDto) {
+        Claims claims = jwtService.decodeJwtToken(useRefreshTokenRequestDto.getRefreshToken());
+
+        if (!claims.getSubject().equals("refresh_token")) {
+            throw new CustomException(HttpStatus.UNAUTHORIZED, "잘못된 요청입니다.");
+        }
+
+        if (authService.getRefreshToken(claims.get("uid", Long.class)) == null) {
+            throw new CustomException(HttpStatus.UNAUTHORIZED, "잘못된 요청입니다.");
+        }
+
+        String accessToken = jwtService.createAccessToken(claims.get("uid", Long.class));
+
+        return new UseRefreshTokenResponseDto(true, accessToken);
     }
 
     @PostMapping("/logout")
     public ResponseEntity<TokenDeleteDto> logout(@RequestBody TokenRequestDto tokenRequestDto) {
         return ResponseEntity.ok(authService.logout(tokenRequestDto));
+    }
+
+    @PutMapping("/password")
+    public MemberResponseDto updatePassword(@AuthenticationPrincipal MemberPrincipal memberPrincipal, @RequestBody UpdatePasswordRequestDto updatePasswordRequestDto) {
+        Member member = authService.updatePassword(memberPrincipal.getMember().getId(), updatePasswordRequestDto);
+        return new MemberResponseDto(true, member.getName());
     }
 
     @PostMapping("/test")
