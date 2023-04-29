14518efb0c8fc9d67534a139c549fad0ac8744e2
List <namest504@gmail.com>
2023-04-30 02:48:05 +0900

    fix : Member 조회 API 수정

--- src/main/java/com/list/nevrytime/controller/MemberController.java
+++ src/main/java/com/list/nevrytime/controller/MemberController.java
@@ -1,9 +1,12 @@
 package com.list.nevrytime.controller;
 
+import com.list.nevrytime.entity.Member;
+import com.list.nevrytime.security.jwt.MemberPrincipal;
 import com.list.nevrytime.service.MemberService;
 import com.list.nevrytime.security.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import static com.list.nevrytime.dto.MemberDto.*;
@@ -15,12 +18,15 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/me")
-    public ResponseEntity<MemberResponseDto> findMemberInfoById(@RequestBody Long memberId) {
-        return ResponseEntity.ok(new MemberResponseDto(true,memberService.findMemberInfoById(memberId).getName()));
+    public MemberResponseDto findMemberInfoById(@AuthenticationPrincipal MemberPrincipal memberPrincipal) {
+        Member member = memberService.findMemberInfoById(memberPrincipal.getMember().getId());
+        System.out.println("member.getNickName() = " + member.getNickName());
+        return new MemberResponseDto(true, member.getName(), member.getNickName());
     }
 
     @GetMapping("/{name}")
-    public ResponseEntity<MemberResponseDto> findMemberInfoByName(@PathVariable String name) {
-        return ResponseEntity.ok(new MemberResponseDto(true ,memberService.findMemberInfoByName(name).getName()));
+    public MemberResponseDto findMemberInfoByName(@PathVariable String name) {
+        Member member = memberService.findMemberInfoByName(name);
+        return new MemberResponseDto(true , member.getName(), member.getNickName());
     }
 }
