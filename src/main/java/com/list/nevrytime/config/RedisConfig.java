a9c8d1740a1e88e7f42812d6fc4a97085e955e0c
List <namest504@gmail.com>
2023-05-02 17:35:56 +0900

    fix : 오류 수정

--- src/main/java/com/list/nevrytime/config/RedisConfig.java
+++ src/main/java/com/list/nevrytime/config/RedisConfig.java
@@ -7,7 +7,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-public class RedisConfig
+public class RedisConfig{
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
