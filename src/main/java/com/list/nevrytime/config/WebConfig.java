bab49201baa7dbcf139954263de658b5213ff12b
List <namest504@gmail.com>
2023-05-04 18:02:59 +0900

    feat : Access-Control-Allow-Headers 설정 추가

--- src/main/java/com/list/nevrytime/config/WebConfig.java
+++ src/main/java/com/list/nevrytime/config/WebConfig.java
@@ -15,6 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
 						HttpMethod.HEAD.name(),
 						HttpMethod.POST.name(),
 						HttpMethod.PUT.name(),
-						HttpMethod.DELETE.name());
+						HttpMethod.DELETE.name())
+				.allowedHeaders("Origin, X-Requested-With, Content-Type, Accept, Authorization" );
 	}
 }
