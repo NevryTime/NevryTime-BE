69c337fcfbc16c2a817ddf3bd696b92e758aaa10
List <61047602+namest504@users.noreply.github.com>
2023-05-06 16:11:53 +0900

    feat : 이미지 다운로드 로직 구현

--- src/main/java/com/list/nevrytime/service/ImageService.java
+++ src/main/java/com/list/nevrytime/service/ImageService.java
@@ -2,9 +2,11 @@ package com.list.nevrytime.service;
 
 import com.list.nevrytime.entity.Content;
 import com.list.nevrytime.entity.Image;
+import com.list.nevrytime.entity.QImage;
 import com.list.nevrytime.exception.CustomException;
 import com.list.nevrytime.repository.ContentRepository;
 import com.list.nevrytime.repository.ImageRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -12,7 +14,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 import static com.list.nevrytime.dto.ImageDto.*;
 
@@ -21,7 +25,7 @@ import static com.list.nevrytime.dto.ImageDto.*;
 @RequiredArgsConstructor
 public class ImageService {
 
-
+    private final JPAQueryFactory jpaQueryFactory;
     private final ImageRepository imageRepository;
     private final ContentRepository contentRepository;
 
@@ -45,12 +49,34 @@ public class ImageService {
     }
 
     // 이미지 파일로 압축하기
-    public DownloadImageResponseDto downloadImage(Long fileId) {
-        Image imageData = imageRepository.findById(fileId)
-                .orElseThrow(RuntimeException::new);
+    public List<DownloadImageResponseDto> downloadImage(Long contentId) {
+        QImage qImage = new QImage("image");
 
-        byte[] file = Base64.getDecoder().decode(imageData.getImageData());
-        String type = imageData.getType();
-        return new DownloadImageResponseDto(file, type);
+        List<Image> images = jpaQueryFactory
+                .selectFrom(qImage)
+                .where(qImage.content.id.eq(contentId))
+                .fetch();
+
+        List<DownloadImageResponseDto> downloadImageResponseDtos = new ArrayList<>();
+
+
+        for (Image image : images) {
+            DownloadImageResponseDto downloadImageResponseDto = new DownloadImageResponseDto(Base64.getDecoder().decode(image.getImageData()), image.getType());
+            downloadImageResponseDtos.add(downloadImageResponseDto);
+        }
+
+//        byte[] file = Base64.getDecoder().decode(imageData.getImageData());
+//        String type = imageData.getType();
+        return downloadImageResponseDtos;
+    }
+
+    // 이미지 파일로 압축하기
+    public DownloadImageResponseDto testDownloadImage(Long fileId) {
+
+        Image image = imageRepository.findById(fileId)
+                .orElseThrow(() -> new CustomException(HttpStatus.BAD_REQUEST, "사진이 없습니다."));
+        byte[] file = Base64.getDecoder().decode(image.getImageData());
+        String type = image.getType();
+        return new DownloadImageResponseDto( file, type);
     }
 }
