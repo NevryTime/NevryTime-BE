00e38ceda19dea5d9c4a66a2f1d47347e353c27c
List <61047602+namest504@users.noreply.github.com>
2023-05-06 14:12:08 +0900

    feat : Image Repository 추가

--- src/main/java/com/list/nevrytime/repository/ImageRepository.java
+++ src/main/java/com/list/nevrytime/repository/ImageRepository.java
@@ -2,6 +2,8 @@ package com.list.nevrytime.repository;
 
 import com.list.nevrytime.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
 }
